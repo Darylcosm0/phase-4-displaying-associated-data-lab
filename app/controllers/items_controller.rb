e15692ffdc26d34365898ed9da2c245bac183fa7
Darylcosm0 <mutaidaryl@gmail.com>
2023-03-20 09:53:08 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,16 @@
 class ItemsController < ApplicationController
-end
+    def index
+      items = Item.includes(:user).all
+      render json: items.map { |item| {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        price: item.price,
+        user: {
+          id: item.user.id,
+          username: item.user.username,
+          city: item.user.city
+        }
+      } }
+    end
+  end
